70cbf9976bb80019f7f2a6875e6e946915e6f255
kevinjoythomas <kevinjoythomas2004@gmail.com>
2024-11-03 21:32:31 +0530

    etl added

--- networkSecurity/exception/exception.py
+++ networkSecurity/exception/exception.py
@@ -6,16 +6,11 @@ class NetworkSecurityException(Exception):
         _, _, exc_tb  = error_details.exc_info()
         
         self.lineno = exc_tb.tb_lineno
-        self.file_name = exc_tb.tb_frame.f_code.filename
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
         
     def __str__(self):
         return "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
             self.file_name, self.lineno, str(self.error_message)
         )
         
-try:
-    logger.logging.info("Enter the try block")
-    a = 1/0
-    # Some code that might cause an exception
-except NetworkSecurityException as e:
-    raise NetworkSecurityException(e, sys)
+
